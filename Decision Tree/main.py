54606202c0e5d659a403394864201678a93fed96
Nitesh Jeganathan <pen2nitesh@gmail.com>
2024-11-20 04:43:29 +0530

    Updated

--- Decision Tree/main.py
+++ Decision Tree/main.py
@@ -1,44 +1,43 @@
-# Importing Libraries
-import numpy as np
+# Importing Libraries 
+import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Importing Dataset
+# Importing Dataset 
 dataset = pd.read_csv('breast-cancer.data')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Label Encoding for Features
+# Label Encoding Features
 les = []
-for i in range(X.shape[1]): 
+for i in range(X.shape[1]):
     le = LabelEncoder()
     X[:, i] = le.fit_transform(X[:, i])
     les.append(le)
 
-# Label Encoding for Target
+# Label Encoding Target
 le_target = LabelEncoder()
 y = le_target.fit_transform(y)
 
 # Splitting Training and Testing Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-# Setting up Models 
+# Setting up Models
 models = {
-    "Random Forest": RandomForestClassifier(), 
-    "Ada Boost": AdaBoostClassifier(DecisionTreeClassifier(), n_estimators=100)
+    "CART": DecisionTreeClassifier(criterion='gini'), 
+    "ID3": DecisionTreeClassifier(criterion='entropy')
 }
 
-# Comparing Performance
+# Comparing Performance 
 accuracy_scores = {}
 
-for model_name, model in models.items(): 
+for model_name, model in models.items():
     model.fit(X_train, y_train)
-
+    
     y_pred = model.predict(X_test)
 
     accuracy_scores[model_name] = accuracy_score(y_test, y_pred)
@@ -46,3 +45,4 @@ for model_name, model in models.items():
 # Plotting Graphs
 plt.bar(accuracy_scores.keys(), accuracy_scores.values())
 plt.show()
+
